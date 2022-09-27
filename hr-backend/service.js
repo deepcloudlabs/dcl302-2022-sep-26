fb8b82335e3b0bdf1ff0f8d3ecac1ccd72746839
Binnur KURT <deepcloudlabs@gmail.com>
2022-09-27 12:30:10 +0300

    Update service.js

--- hr-backend/service.js
+++ hr-backend/service.js
@@ -14,7 +14,46 @@ const mongo_opts = {
 mongoose.connect(mongodb_url, mongo_opts);
 
 const employeeSchema = new mongoose.Schema({
-
+    "_id": {
+        type: String,
+        required: true
+    },
+    "fullname": {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    "identityNo": {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [
+            tcKimlikNoValidator,
+            "You must provide a valid identity no."
+        ]
+    },
+    "salary": {
+        type: Number,
+        required: true,
+        min: 5500
+    },
+    "iban": {
+        type: String,
+        required: true,
+        validate: [
+            ibanValidator,
+            "You must provide a valid iban."
+        ]
+    },
+    "birthYear": {
+        type: Number,
+        required: true
+    },
+    "photo": {
+        type: String,
+        required: false,
+        default: AppConfig.NO_IMAGE
+    }
 });
 
 // Model -> Employee
